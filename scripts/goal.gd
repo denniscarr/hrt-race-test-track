8d39413b08d84100cc0f9a0e85756456b1515374
Ben Sironko <sironkbd@gmail.com>
2026-05-08 05:20:10 -0700

    reworked close and far to carrots bets, added distance UI, added on_hover_enter/on_hover_exit functions to bets to use with bet cards

--- scripts/goal.gd
+++ scripts/goal.gd
@@ -8,8 +8,26 @@ signal grabbed_by_horse(horse_who_grabbed_me: Horse)
 @export_category("Node References")
 
 @export var _sprite: Sprite2D
+@export var _near_radius: float = 100
+@export var _far_radius: float = 200
+
+var _near_radius_squared: float = -1
+var near_radius_squared: float:
+	get:
+		if _near_radius_squared < 0:
+			_near_radius_squared = pow(_near_radius, 2)
+		return _near_radius_squared
+
+var _far_radius_squared: float = -1
+var far_radius_squared: float:
+	get:
+		if _far_radius_squared < 0:
+			_far_radius_squared = pow(_far_radius, 2)
+		return _far_radius_squared
 
 var _eaten: bool = false
+var _distance_circle_on: bool
+var _far_distance: bool
 
 
 func _ready() -> void:
@@ -29,3 +47,20 @@ func _physics_process(delta: float) -> void:
 			# Get eaten by a horse
 			grabbed_by_horse.emit(body)
 			_eaten = true
+
+
+func toggle_distance_circle(toggle_on: bool, far_distance: bool):
+	_distance_circle_on = toggle_on
+	_far_distance = far_distance
+	queue_redraw()
+
+
+func _draw():
+	if _distance_circle_on:
+		var _light_blue: Color = Color("478cbf", 0.5)
+		draw_circle(
+			Vector2(0, 0),
+			\
+			_far_radius if _far_distance else _near_radius,
+			_light_blue,
+		)
